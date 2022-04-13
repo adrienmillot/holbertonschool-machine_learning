6fea7eaad33709eecc242ccad5f34dcca25c8adc
Adrien <adrien.millot@gmail.com>
2022-04-13 15:16:48 +0200

    Howdy Partner

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Concatenates two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+        Returns the concatenation of two arrays
+    """
+
+    return arr1 + arr2
