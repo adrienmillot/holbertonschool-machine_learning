6e8b45ba16b65ab9df15daf76ae16edd24856a71
Adrien <adrien.millot@gmail.com>
2022-04-13 15:50:10 +0200

    I’ll Use My Scale

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Calculates the shape of a numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    """
+        Returns the shape of a matrix
+    """
+
+    return matrix.shape
