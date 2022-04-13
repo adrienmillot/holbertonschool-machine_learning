3089021c9f30fde70e6ff8a5508c2ad0d379bee4
Adrien <adrien.millot@gmail.com>
2022-04-13 16:00:59 +0200

    Saddle Up

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Performs matrix multiplication
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+        Returns the result of the matrix product of two matrices
+    """
+
+    return np.matmul(mat1, mat2)
