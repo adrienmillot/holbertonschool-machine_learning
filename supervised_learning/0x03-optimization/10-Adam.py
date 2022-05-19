27dee27cf7d1abc9453d479eb701e58d24bcd472
Adrien <adrien.millot@gmail.com>
2022-05-19 17:30:48 +0200

    Adam Upgraded

---
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import tensorflow.compat.v1 as tf
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """
+        Creates the operation to perform the Adam optimization
+
+        Args:
+            loss: loss of the network
+            alpha: learning rate
+            beta1: first Adam weight
+            beta2: second Adam weight
+            epsilon: small number to avoid division by zero in Adam
+
+        Returns:
+            train_op: the Adam optimization operation
+    """
+
+    return tf.train.AdamOptimizer(
+        learning_rate=alpha,
+        beta1=beta1,
+        beta2=beta2,
+        epsilon=epsilon
+    ).minimize(loss)
