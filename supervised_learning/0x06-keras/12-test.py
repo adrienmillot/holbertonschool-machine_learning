014c39914d9bbf77f0bc2f014b90baa1bf962a2a
Adrien <adrien.millot@gmail.com>
2022-05-31 15:19:58 +0200

    Test

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+    Script to test a model using mini-batch gradient descent
+"""
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+        Tests a model using mini-batch gradient descent
+
+        Args:
+            network: model to test
+            data: data to test the model
+            labels: labels of data
+            verbose: print information about the testing or not
+
+        Returns:
+            the tested model
+    """
+
+    return network.evaluate(
+        data,
+        labels,
+        verbose=verbose,
+    )
