d7837b9e332a5c64575ca59d22d434cf6e970262
Adrien <adrien.millot@gmail.com>
2022-06-02 16:13:54 +0200

    ame Convolution

---
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+    Performs a same convolution on grayscale images
+"""
+
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """
+        Function that performs a convolution on grayscale images with
+        same padding
+
+        Args:
+            images: np.ndarray with shape (m, h, w) containing multiple
+                    grayscale images
+                m: the number of images
+                h: height in pixels of the images
+                w: width in pixels of the images
+            kernel: np.ndarray with shape (kh, kw) containing the kernel
+                    for the convolution
+                kh: kernel height
+                kw: kernel width
+            Returns: np.ndarray containing the convolved images
+    """
+
+    image_nb, image_height, image_width = images.shape
+    kernel_height, kernel_width = kernel.shape
+    image_size = np.arange(image_nb)
+
+    # Calculate the padding height and width
+    padding_height = int((kernel_height - 1) / 2)
+    padding_width = int((kernel_width - 1) / 2)
+
+    if kernel_height % 2 == 0:
+        padding_height += 1
+    if kernel_width % 2 == 0:
+        padding_width += 1
+
+    # Add padding to the images
+    padding = np.pad(
+        images,
+        pad_width=(
+            (0, 0),
+            (padding_height, padding_height),
+            (padding_width, padding_width)
+        ),
+        mode='constant',
+    )
+
+    # Create a new matrice to hold the padded images
+    convoluted_images = np.zeros((image_nb, image_height, image_width))
+
+    # Perform the convolution
+    for row in range(image_height):
+        for column in range(image_width):
+            input_image = padding[
+                image_size,
+                row:row + kernel_height,
+                column:column + kernel_width
+            ]
+
+            convoluted_images[image_size, row, column] = np.sum(
+                input_image * kernel,
+                axis=(1, 2)
+            )
+
+    return convoluted_images
